934bad8ba9c85bb76cf92309c772bc706b736644
ms29ash <ms29ash@gmail.com>
2022-05-03 10:40:40 +0530

    add icons and delete unused files

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 import './index.css';
 import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.createRoot(document.getElementById('root'))
   .render(<React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+
+    <App />
+
   </React.StrictMode>)
 
 
